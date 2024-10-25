c1d5e671cdb9c294c152b26156b37135cc428de1
Kiran9223 <88050617+Kiran9223@users.noreply.github.com>
2024-10-25 16:28:06 -0700

    Added protected routes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import jwt
 import datetime
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
+from functools import wraps
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '14a0458f42584319bdeed320286f6dd5'
@@ -16,6 +17,27 @@ mysql = MySQL(app)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+def protected_route(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('x-access-token')
+        
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 403
+        
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            if data.get('role') != 'admin':
+                return jsonify({'message': 'Admin access required!'}), 403
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token has expired!'}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({'message': 'Invalid token!'}), 401
+        
+        return f(*args, **kwargs)
+    
+    return decorated
 
 @app.route('/get-users', methods=['GET'])
 def get_users():
@@ -32,6 +54,7 @@ def login():
     if auth and auth.get('username') == 'admin' and auth.get('password') == 'password':
         token = jwt.encode({
             'username': auth['username'],
+            'role': 'admin',
             'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)  # Token expires in 30 minutes
         }, app.config['SECRET_KEY'], algorithm='HS256')
 
@@ -39,20 +62,20 @@ def login():
 
     return jsonify({'message': 'Invalid credentials!'}), 401
 
-@app.route('/protected', methods=['GET'])
-def protected():
-    token = request.headers.get('Authorization')
+@app.route('/update-book', methods=['PUT'])
 
-    if not token:
-        return jsonify({'message': 'Token is missing!'}), 403
+@app.route('/add-book', methods=['POST'])
+@protected_route
+def add_book():
+    cur = mysql.connection.cursor()
+    data = request.get_json()
+    cur.execute("INSERT INTO books (id, name, description) VALUES (%s, %s, %s)", (data['id'], data['name'], data['description']))
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({'message': 'Book added!', 'data': data})
 
-    try:
-        data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        return jsonify({'message': f'Welcome, {data["username"]}!'})
-    except jwt.ExpiredSignatureError:
-        return jsonify({'message': 'Token has expired!'}), 403
-    except jwt.InvalidTokenError:
-        return jsonify({'message': 'Invalid token!'}), 403
+
+
 
 
 if __name__ == '__main__':
